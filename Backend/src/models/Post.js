e1344903994c626b6956db4f029fd7ccf49ce54d
TvT <toantran114477@gmail.com>
2025-06-20 18:24:36 +0700

    Change Model Post

--- Backend/src/models/Post.js
+++ Backend/src/models/Post.js
@@ -2,10 +2,16 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  content: { type: String, required: true },
   excerpt: { type: String },
-  categories: [{ type: String }], // Array of category names (e.g., ["Minimalism", "Lifestyle"])
+  categories: [{ type: String }], // Array of category names (e.g., ["Lifestyle", "Productivity"])
   image: { type: String },
+  sections: [
+    {
+      subtitle: { type: String, required: true },
+      content: { type: String, required: true },
+      image: { type: String }, // Optional image URL for each section
+    },
+  ],
   createdAt: { type: Date, default: Date.now },
   readingTime: { type: Number },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
